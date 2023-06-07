87ac8506924140aafbfaf919640626bad73e6c02
ramirez <prlara2000@gmail.com>
2023-06-06 23:10:45 -0500

    add deployment

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function () {
+  $('.carousel').carousel({ interval: 3000 });
+  $('#carouselButton').click(function () {
+    if ($('#carouselButton').children('i').hasClass('fa-pause')) {
+      $('.carousel').carousel('pause');
+      $('#carouselButton').children('i').removeClass('fa-pause');
+      $('#carouselButton').children('i').addClass('fa-play');
+      $('.play-pause').removeClass('btn-secondary');
+      $('.play-pause').addClass('btn-success');
+    } else {
+      $('.carousel').carousel('cycle');
+      $('#carouselButton').children('i').removeClass('fa-play');
+      $('#carouselButton').children('i').addClass('fa-pause');
+      $('.play-pause').removeClass('btn-success');
+      $('.play-pause').addClass('btn-secondary');
+    }
+  });
+});
